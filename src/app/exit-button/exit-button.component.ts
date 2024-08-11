77ba152183f0d87b4197f519669ae81d1246ff3e
Aviaaaaa <155906600+Aviaaaaa@users.noreply.github.com>
2024-08-11 22:29:56 +0300

    exit-button

---
+++ src/app/exit-button/exit-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-exit-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './exit-button.component.html',
+  styleUrl: './exit-button.component.css'
+})
+export class ExitButtonComponent {
+
+}
