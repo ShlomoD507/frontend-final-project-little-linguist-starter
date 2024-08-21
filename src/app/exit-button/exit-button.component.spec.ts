0adc10bf2e0541cb39d000c0a23efcde9dfb7a23
Shlomi <shldahan@hotmail.com>
2024-08-21 21:32:48 +0300

    update

--- src/app/exit-button/exit-button.component.spec.ts
+++ src/app/exit-button/exit-button.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ExitButtonComponent } from './exit-button.component';
 
 describe('ExitButtonComponent', () => {
--- src/app/mixd-words/mixd-words.component.ts
+++ src/app/mixd-words/mixd-words.component.ts
@@ -4,20 +4,30 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ExitButtonComponent } from '../exit-button/exit-button.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-mixd-words',
   standalone: true,
   imports: [
-    CommonModule,RouterModule,ExitButtonComponent
+    CommonModule,RouterModule,ExitButtonComponent,FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule
   ],
   templateUrl: './mixd-words.component.html',
   styleUrl: './mixd-words.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MixdWordsComponent implements OnInit{ 
+isLoading = true;
+
+
+
+
   @Input()
   id=''
+
 
   currentCategory: Category | undefined;
   constructor(private CategoriesService:CategoriesService){}
