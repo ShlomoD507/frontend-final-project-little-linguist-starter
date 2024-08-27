af328459effd12074974aa08bce9279ec31b55b0
Shlomi <shldahan@hotmail.com>
2024-08-27 20:38:26 +0300

    exit button working

--- src/app/exit-dialog/exit-dialog.component.ts
+++ src/app/exit-dialog/exit-dialog.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-exit-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule
+  ],
   templateUrl: './exit-dialog.component.html',
   styleUrl: './exit-dialog.component.css',
 })
